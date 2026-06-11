0129772cfdbce9be2ba88b46b9c405d18b455ada
Allyfeng2007 <fengguier@126.com>
2026-06-11 09:17:21 +0800

    Update firstone.py

--- firstone.py
+++ firstone.py
@@ -7,29 +7,33 @@ client = OpenAI(
 
 def judge_level(text):
     response = client.chat.completions.create(
-        model="deepseek-chat",
+        model="deepseek-v4-pro",
         messages=[
-            {"role": "system", "content": "### 定位：语义歧视分析专家\n ### 任务：请对用户输入的句子进行歧视性分析，并用 1 到 5 之间的数字表示其歧视程度。1 表示没有歧视，5 表示极为歧视。\n ###输出 ：只输出数字，不需要额外解释。"},
+            {"role": "system", "content": '### 定位：语义歧视分析专家\n ### 任务：请对用户输入的句子进行歧视性分析，并用 1 到 5 之间的数字表示其歧视程度。1 表示没有歧视，5 表示极为歧视。\n ###输出 ：只输出数字，不需要额外解释。'},
             {"role": "user", "content": text},
         ],
-        temperature=0.7
+        stream=False,
+        reasoning_effort="high",
+        extra_body={"thinking": {"type": "enabled"}}
     )
+
     return response.choices[0].message.content
 
 
 def tiao_zheng(text):
     response = client.chat.completions.create(
-        model="deepseek-chat",
+        model="deepseek-v4-pro",
         messages=[
             {"role": "system", "content": "### 定位：语言表述专家\n ### 任务：将歧视性语句换一种方法表述，使表述中不包含歧视语义。"},
             {"role": "user", "content": text},
         ],
-        temperature=0.7
+        stream=False,
+        reasoning_effort="high",
+        extra_body={"thinking": {"type": "enabled"}}
     )
-    return response.choices[0].message.content
 
 
-st.set_page_config(page_title='我的第一个网页')
+st.set_page_config(page_title='语义转换')
 st.title('💕💕💕💕语言检测及纠正')
 user_input = st.text_area('请输入要发言的句子：', height=100)
 if st.button('开始分析'):
